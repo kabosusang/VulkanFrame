22f85d20af327c5c6bcfd1480b8ae56122301601
kabosusang <375487345@qq.com>
2023-09-29 20:55:42 +0800

    Integrating Dear ImGui in Vulkan

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,38 @@
-#define GLFW_INCLUDE_VULKAN
-#include "GLFW/glfw3.h"
+#include "VKimgui.h"
+#include "VulkanRenderer.h"
 
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
+int main(){
+    glfwSetErrorCallback(glfw_error_callback);
+    if (!glfwInit())
+        return 1;
 
-#include "iostream"
-
-int main()
-{
-    glfwInit();
-
-    glfwWindowHint(GLFW_CLIENT_API,GLFW_NO_API);
-    GLFWwindow* window = glfwCreateWindow(800,600,"Vulkan_window"
-    ,nullptr,nullptr);
-
-    uint32_t extensionCount = 0;
-    vkEnumerateInstanceExtensionProperties(nullptr,
-                                        &extensionCount,nullptr);
-    std::cout << extensionCount <<"_extensions_supported" << std::endl;                 
-
-    glm::mat4 matrix;
-    glm::vec4 vec;
-    auto test = matrix * vec;
-    while (~glfwWindowShouldClose(window)){
-        glfwPollEvents();
+    // Create window with Vulkan context
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "Dear ImGui GLFW+Vulkan example", nullptr, nullptr);
+    if (!glfwVulkanSupported())
+    {
+        printf("GLFW: Vulkan Not Supported\n");
+        return 1;
     }
 
+    // Main loop
+    //Vulkan Renderer
+    HelloTriangleApplication app;
+
+    app.InputWindow(window);
+    
+    //Imugi Renderer
+    
+    try{
+        app.run();
+    }catch(const std::exception&e){
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    } 
+    
     glfwDestroyWindow(window);
     glfwTerminate();
-    
-    return 0;
+    return EXIT_SUCCESS;
+
+
 }
